e5e92c6ca7d72f819e1a8a7f28903a2a9725f6d4
Waffen Sultan <waffen.sultan@gmail.com>
2025-09-22 17:20:06 +0800

    feat: add return type

--- src/app/api/hotlines/route.ts
+++ src/app/api/hotlines/route.ts
@@ -1,10 +1,13 @@
 import { NextResponse } from 'next/server';
 import { promises as fs } from 'fs';
+import { IHotlinesResponse } from '@/interfaces/IHotlines';
 
+// fetching hotlines for CITIES
 export async function GET() {
   try {
     const file = await fs.readFile(process.cwd() + '/src/data/hotlines.json', 'utf8');
-    const data = JSON.parse(file);
+    const data: IHotlinesResponse = JSON.parse(file);
+
     return NextResponse.json(data);
   } catch {
     return NextResponse.json({ error: 'Failed to load hotlines' }, { status: 500 });
