93e8cac0f2dd1fd1f4aed442aa88d22984823e19
Waffen Sultan <waffen.sultan@gmail.com>
2025-09-22 17:20:35 +0800

    feat: update UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ import { useState, useEffect } from 'react';
 
 export default function RootPage() {
   const [metadata, setMetadata] = useState<IMetadata | null>(null);
+  const [selectedRegion, setSelectedRegion] = useState('Region IV-A');
 
   // Fetch initial data
   useEffect(() => {
@@ -29,40 +30,48 @@ export default function RootPage() {
     fetchMetadata();
   }, []);
 
+  const availableCitiesOnRegion = metadata?.regions.find(region => region.code === selectedRegion);
+
   return (
     <div>
-      <div className="relative bg-gradient-to-br from-orange-600 to-yellow-300 min-h-[50vh] md:min-h-[30vh] flex flex-col justify-center px-4 md:px-32 pb-8">
-        {/* Main Title */}
+      <div className="relative bg-blue-800 min-h-[38vh] md:min-h-[35vh] flex flex-col justify-center px-4 md:px-32">
         <div className="flex flex-col space-y-3">
           <h1 className="text-3xl md:text-6xl font-bold text-white leading-tight">
             Philippines Emergency Hotlines
           </h1>
           <div className="flex flex-col items-start">
-            <p className="text-white/90 md:text-lg tracking-tight">
-              Quick access to emergency services across the Philippines.
+            <p className="text-white/90 md:text-lg tracking-tight md:hidden">
+              Quick access to emergency services across the Philippines. Find police, fire, medical,
+              and government hotlines in your area.
             </p>
-            <p className="text-white/90 md:text-lg tracking-tight">
+            <p className="hidden md:flex text-white/90 md:text-lg tracking-tight">
               Find police, fire, medical, and government hotlines in your area.
+            </p>
+            <p className="hidden md:flex text-white/90 md:text-lg tracking-tight">
+              Quick access to emergency services across the Philippines.
             </p>
           </div>
         </div>
       </div>
 
       {/* SEARCH INPUT */}
-      <div className="flex justify-center items-center px-4 md:px-32 py-6 -mt-12 relative z-10">
+      <div className="flex justify-center items-center px-4 md:px-32 py-6 -mt-13 relative z-10">
         <div className="flex flex-col gap-5 w-full max-w-6xl">
           <input
             type="text"
             placeholder="Search by hotline, city, or region..."
-            className="w-full px-6 py-4 text-lg border-2 border-gray-300 rounded-full focus:outline-none focus:border-orange-500 transition-colors shadow-lg bg-white"
+            className="w-full px-6 py-4 text-lg border-2 border-gray-300 rounded-full focus:outline-none focus:border-blue-900 transition-colors shadow-lg bg-white"
           />
 
           {/* TODO: Update this so that it uses the selected region and then shows its respective cities (use a state for this) */}
           {metadata ? (
-            <div className="flex flex-row items-center justify-center gap-5">
-              <Select defaultValue={metadata.regions[0].code}>
-                <SelectTrigger className="w-[250px] py-5 bg-white text-lg font-normal border-gray-300 rounded-full">
-                  <SelectValue placeholder={`Region: ${metadata.regions[0].code} test`} />
+            <div className="flex flex-row items-center justify-center gap-2 sm:gap-5 px-2 sm:px-6 w-full">
+              <Select
+                defaultValue={selectedRegion}
+                onValueChange={value => setSelectedRegion(value)}
+              >
+                <SelectTrigger className="flex-1 min-w-0 sm:flex-none sm:w-[250px] py-5 bg-white text-sm sm:text-lg font-normal border-gray-300 rounded-full">
+                  <SelectValue placeholder={`Region: ${metadata.regions[0].code}`} />
                 </SelectTrigger>
                 <SelectContent className="text-lg border-gray-300">
                   {metadata.regions.map(region => (
@@ -73,12 +82,12 @@ export default function RootPage() {
                 </SelectContent>
               </Select>
 
-              <Select defaultValue={metadata.regions[0].cities[0]}>
-                <SelectTrigger className="w-[250px] py-5 bg-white text-lg font-normal border-gray-300 rounded-full">
-                  <SelectValue placeholder={`City: ${metadata.regions[0].cities[0]}`} />
+              <Select defaultValue={availableCitiesOnRegion?.cities[0] || ''}>
+                <SelectTrigger className="flex-1 min-w-0 sm:flex-none sm:w-[250px] py-5 bg-white text-sm sm:text-lg font-normal border-gray-300 rounded-full">
+                  <SelectValue placeholder={`City: ${availableCitiesOnRegion?.cities[0] || ''}`} />
                 </SelectTrigger>
                 <SelectContent className="text-lg border-gray-300">
-                  {metadata.regions[0].cities.map(city => (
+                  {availableCitiesOnRegion?.cities.map(city => (
                     <SelectItem key={city} value={city}>
                       City: {city}
                     </SelectItem>
@@ -87,7 +96,7 @@ export default function RootPage() {
               </Select>
 
               <Select defaultValue={metadata.hotlineTypes[0]}>
-                <SelectTrigger className="w-[270px] py-5 bg-white text-lg font-normal border-gray-300 rounded-full">
+                <SelectTrigger className="flex-1 min-w-0 sm:flex-none sm:w-[270px] py-5 bg-white text-sm sm:text-lg font-normal border-gray-300 rounded-full">
                   <SelectValue placeholder={`Service: ${metadata.hotlineTypes[0]}`} />
                 </SelectTrigger>
                 <SelectContent className="text-lg border-gray-300">
