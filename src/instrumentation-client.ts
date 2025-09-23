dbbfaf80774bae2c0caa9f99373c4e104df4204a
Waffen Sultan <waffen.sultan@gmail.com>
2025-09-23 17:05:51 +0800

    feat: add exception handling for posthog analytics

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -2,6 +2,10 @@
 
 import posthog from 'posthog-js';
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
-});
+if (!process.env.NEXT_PUBLIC_POSTHOG_KEY || !process.env.NEXT_PUBLIC_POSTHOG_HOST) {
+  console.error('Posthog environment variables not found!');
+} else {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  });
+}
